d10ece5cc10da072d078d70a57c595f029d76d48
babuush <thanapatpiyamassakul@gmail.com>
2024-12-07 18:57:03 +0700

    feat: check session

--- createPost.php
+++ createPost.php
@@ -1,1 +1,9 @@
 <?php
+session_start();
+require_once 'databaseConnect.php';
+
+// Check if user is logged in or not authorized
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'company') {
+    header("Location: login.php");
+    exit();
+}
