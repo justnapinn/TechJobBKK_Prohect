785b682c3ab0a042e3b0367bfb7b2ca4087fe411
babuush <thanapatpiyamassakul@gmail.com>
2024-12-07 18:52:14 +0700

    feat: check session and get session info

--- profile.php
+++ profile.php
@@ -3,13 +3,12 @@ session_start();
 require_once 'databaseConnect.php';
 
 // Check if user is logged in
-//if (!isset($_SESSION['user_id'])) {
-//    header("Location: login.php");
-//    exit();
-//}
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
-$user_id = '1';
-//$user_id = $_SESSION['user_id'];
+$user_id = $_SESSION['user_id'];
 $error_message = '';
 $success_message = '';
 
