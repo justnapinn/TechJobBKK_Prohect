8c54e33e3d22a0fab0e012dd704a2272846a8c58
babuush <thanapatpiyamassakul@gmail.com>
2024-12-08 21:10:48 +0700

    feat: check login session

--- applicants.php
+++ applicants.php
@@ -3,3 +3,9 @@ session_start();
 require_once 'databaseConnect.php';
 include('navbar.php');
 
+// Check if the user is logged in and has 'company' user_type
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'company') {
+    header("Location: login.php");
+    exit();
+}
+
