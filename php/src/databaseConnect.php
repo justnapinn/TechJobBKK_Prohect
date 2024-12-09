bb4447267681e3361833577f32b3f73d3cf3c0c9
babuush <thanapatpiyamassakul@gmail.com>
2024-12-10 04:03:50 +0700

    refactor: change database configuration

--- php/src/databaseConnect.php
+++ php/src/databaseConnect.php
@@ -1,12 +1,12 @@
 <?php
 // Database Configuration
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'final_de251');
+$host = 'db';
+$user = 'MYSQL_USER';
+$pass = 'MYSQL_PASSWORD';
+$db = 'MYSQL_DATABASE';
 
 // Create connection
-$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
