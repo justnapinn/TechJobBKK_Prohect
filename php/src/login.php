78cb2584024ddb596dff9bad056bc9dd5fe1803d
babuush <thanapatpiyamassakul@gmail.com>
2024-12-10 04:01:33 +0700

    feat: Buffer Output

--- php/src/login.php
+++ php/src/login.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 require_once 'databaseConnect.php';
 include('navbar.php');
@@ -40,6 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->close();
     $conn->close();
 }
+ob_end_flush();
 ?>
 
 <!DOCTYPE html>
